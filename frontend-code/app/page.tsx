63994b2ef743ef6d4b951a97e8a0bbc4bf04b524
SpandanM110 <spandanmukherjee2003@gmail.com>
2024-03-15 23:06:21 +0530

    Mac and scroll effects

--- frontend-code/app/page.tsx
+++ frontend-code/app/page.tsx
@@ -1,13 +1,23 @@
-import {BentoGridDemo} from './grid';
-
-
+// import { GlobeDemo } from './ggs';
+// import { BentoGridDemo } from './grid';
+import { MacbookScrollDemo } from './mac';
+import { StickyScroll } from './sticksc';
 
 export default function Home() {
+  // Define some content for the StickyScroll component
+  const content = [
+    { id: 1, title: "Card 1", description: "Description for card 1", content: "Content for card 1" },
+    { id: 2, title: "Card 2", description: "Description for card 2", content: "Content for card 2" },
+    { id: 3, title: "Card 3", description: "Description for card 3", content: "Content for card 3" },
+    { id: 4, title: "Card 3", description: "Description for card 4", content: "Content for card 4" }
+
+  ];
+
   return (
     <main>
-     
-      <BentoGridDemo />
-
+      <MacbookScrollDemo />
+      {/* Pass the content array as a prop to the StickyScroll component */}
+      <StickyScroll content={content} />
     </main>
   );
 }
