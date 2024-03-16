ff83f94684d506c002f80d14ebf9a30bf1505dcd
Ojas Aklecha <ojasaklechayt@gmail.com>
2024-03-16 16:43:36 +0530

    Admin List NFT Form

---
+++ frontend-code/app/admin/list-nft/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {SignupFormDemo} from "@/app/user/profile/sign";
+import { NavbarDemo } from "@/app/nav";
+import Footer from "@/app/components/Footer";
+
+
+const YourPage = () => {
+    const mapCount = 5;
+    return (
+        <div>
+            <div className="pb-11">
+                <NavbarDemo />
+            </div>
+            <div className="py-10">
+                <SignupFormDemo />
+                
+            </div>
+      
+        </div>
+
+    );
+};
+
+export default YourPage;
